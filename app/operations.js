c52a6f8436cbddc8a8b11107e8814e3d412ad829
geraJuarez <geratrex781@gmail.com>
2018-09-26 22:44:56 -0500

    Mocha framework for unit testing

---
+++ app/operations.js
@@ -0,0 +1,13 @@
+exports.sum = function(x, y) {
+    return x + y;
+};
+
+exports.substract = function(x, y) {
+    return x - y;
+};
+
+/*
+    function sum(x, y) {
+        return x + y;
+    };
+*/
