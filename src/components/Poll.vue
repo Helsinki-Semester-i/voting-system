b133f6b90559983e69f68ad98f2871c1e2b1af71
geraJuarez <geratrex781@gmail.com>
2018-10-30 14:11:14 -0600

    Added test components

---
+++ src/components/Poll.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div>
+      <p>{{poll}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import api from '@/api';
+
+export default {
+  data() {
+    return {
+      poll: {},
+    };
+  },
+  async created() {
+    this.getPollData();
+  },
+  methods: {
+    async getPollData() {
+      let id = this.$route.params.id;
+      //console.log(id);
+      this.poll = await api.test_getSinglePoll();
+    },
+  },
+};
+</script>
