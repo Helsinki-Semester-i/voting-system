0c13bd818e1b99eb8389fc95c6eceb0d060707dc
geraJuarez <geratrex781@gmail.com>
2018-10-30 14:11:34 -0600

    Add test data

--- src/api.js
+++ src/api.js
@@ -24,4 +24,28 @@ export default {
   getParts() {
     return this.execute('get', '/parts');
   },
+  test_getSinglePoll() {
+    return POLLTEST;
+  }
+};
+
+const VOTES_FOR_POLL_TEST = [
+  {
+    id: 1,
+    question: 'Do you agree with rule 1?',
+    open: true,
+    answers: {
+      1: 'Agree',
+      2: 'Disagree',
+      3: 'Neutral',
+    },
+  }
+];
+
+const POLLTEST = {
+  id: 1,
+  description: 'This is a test Poll',
+  participants: [],
+  anonymous: true,
+  questions: VOTES_FOR_POLL_TEST,
 };
