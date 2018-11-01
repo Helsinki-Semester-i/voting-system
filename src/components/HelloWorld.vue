98c3ee1cdef836adc70795e7e8c14f9d2ccf0e06
geraJuarez <geratrex781@gmail.com>
2018-11-01 00:47:51 -0600

    Add constants to home page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,32 +12,25 @@
       <div class="col s3">
         <div class="card blue-grey darken-1">
           <div class="card-content white-text">
-            <span class="card-title">Aprénde a votar</span>
-            <p>
-              Te enseñaremos a utilizar la plataforma para que aprendas a navegar
-               dentro de las distintas fases del proceso de votación.
-            </p>
+            <span class="card-title">{{demoTitleVote}}</span>
+            <p>{{demoBodyVote}}</p>
           </div>
           <div class="card-action">
             <router-link
             to="/polls">
-            Demo para votar
+            {{demoLinkVote}}
             </router-link>
           </div>
         </div>
         <div class="card blue-grey darken-1">
           <div class="card-content white-text">
-            <span class="card-title">Aprénde a ver los resultados</span>
-            <p>
-              Te enseñaremos a utilizar la plataforma para ver los resultados de una votación
-               y verificar tu voto con el código único que se te proporciona al finalizar
-               una votación.
-            </p>
+            <span class="card-title">{{demoTitleResults}}</span>
+            <p>{{demoBodyResults}}</p>
           </div>
           <div class="card-action">
             <router-link
             to="/result/1">
-            Demo para ver resultados
+            {{demoLinkResults}}
             </router-link>
           </div>
         </div>
@@ -45,3 +38,20 @@
     </div>
   </div>
 </template>
+
+<script>
+import HomeData from '../utils/strings';
+
+export default {
+  data() {
+    return {
+      demoTitleVote: HomeData.HOME_PAGE.demoTitleVote,
+      demoBodyVote: HomeData.HOME_PAGE.demoBodyVote,
+      demoLinkVote: HomeData.HOME_PAGE.demoLinkVote,
+      demoTitleResults: HomeData.HOME_PAGE.demoTitleResults,
+      demoBodyResults: HomeData.HOME_PAGE.demoBodyResults,
+      demoLinkResults: HomeData.HOME_PAGE.demoLinkResults,
+    };
+  },
+};
+</script>
---
+++ src/utils/strings.js
@@ -0,0 +1,10 @@
+export default Object.freeze({
+  HOME_PAGE: {
+    demoTitleVote: 'Aprénde a votar',
+    demoBodyVote: 'Te enseñaremos a utilizar la plataforma para que aprendas a navegar dentro de las distintas fases del proceso de votación.',
+    demoLinkVote: 'Demo para votar',
+    demoTitleResults: 'Aprénde a ver los resultados',
+    demoBodyResults: 'Te enseñaremos a utilizar la plataforma para ver los resultados de una votación y verificar tu voto con el código único que se te proporciona al finalizar una votación.',
+    demoLinkResults: 'Demo para ver resultados',
+  },
+});
