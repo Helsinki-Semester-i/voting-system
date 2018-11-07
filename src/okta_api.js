c06c418068278fbfba95a701ea413232603186cf
geraJuarez <geratrex781@gmail.com>
2018-11-07 10:55:59 -0600

    Apporach to create users on forntend TO DO: * Move the api call to the backend

--- src/okta_api.js
+++ src/okta_api.js
@@ -1,13 +1,25 @@
-import okta from '@okta/okta-sdk-nodejs';
+import axios from 'axios';
 
-const client = new okta.Client({
-  orgUrl: process.env.VUE_APP_AUTH_BASE_URL,
-  token: process.env.VUE_APP_OKTA_TOKEN,
-  requestExecutor: new okta.DefaultRequestExecutor(),
+const client = axios.create({
+  baseURL: process.env.VUE_APP_OKTA,
+  json: true,
 });
 
 export default {
-  async postUser(fname, lname, email) {
+  async execute(method, resource, data) {
+    return client({
+      method,
+      url: resource,
+      data,
+      params: {
+        activate: false,
+      },
+      headers: {
+        Authorization: `SSWS ${process.env.VUE_APP_OKTA_TOKEN}`,
+      },
+    }).then(req => req.data).catch(error => error);
+  },
+  postUser(fname, lname, email) {
     const newOktaUser = {
       profile: {
         firstName: fname,
@@ -16,7 +28,59 @@ export default {
         login: email,
       },
     };
-    client.createUser(newOktaUser, { activate: false })
-      .then(user => user);
+    return this.execute('post', '/users', newOktaUser);
   },
 };
+/*
+const client = new okta.Client({
+  orgUrl: process.env.VUE_APP_AUTH_BASE_URL,
+  token: process.env.VUE_APP_OKTA_TOKEN,
+  requestExecutor: new okta.DefaultRequestExecutor(),
+});
+
+async postUser(fname, lname, email) {
+    const newOktaUser = {
+      profile: {
+        firstName: fname,
+        lastName: lname,
+        email,
+        login: email,
+      },
+    };
+    await client.createUser(newOktaUser, { activate: false })
+      .then(user => user);
+  },
+*/
+
+/* axios request
+axios.post('https://dev-749221.oktapreview.com/api/v1/users', {
+  {
+    profile: {
+      firstName: fname,
+      lastName: lname,
+      email,
+      login: email,
+    },
+  },
+  params: {
+    activate: false,
+  },
+})
+.then(user => user);
+*/
+
+/* no credentials
+curl -v -X POST \
+-H "Accept: application/json" \
+-H "Content-Type: application/json" \
+-H "Authorization: SSWS ${api_token}" \
+-d '{
+  "profile": {
+    "firstName": "Isaac",
+    "lastName": "Brock",
+    "email": "isaac.brock@example.com",
+    "login": "isaac.brock@example.com",
+    "mobilePhone": "555-415-1337"
+  }
+}' "https://dev-749221.oktapreview.com/api/v1/users?activate=false"
+*/
